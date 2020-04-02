651b5e938db374770f9f4cbed6a858f4e6e96663
Jashema Panapa <jcpanapa@gmail.com>
2020-04-02 15:01:36 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,7 +4,9 @@ def third_challenge
        :patriarch => {
          :name "Lord Montague", :age "53"
        }
-       :matriarch => {}
+       :matriarch => {
+         :name "Lady Montague", :age "54"
+       }
        :hero => {}
        :hero_friends => []
      }
